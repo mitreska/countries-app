d920302f715d337c648b5354ff78122aa09e42f3
mitreska <estudio.mtp@gmail.com>
2022-08-23 00:39:25 -0300

    initial commit
    creating project;
    creating podfile;
    
    init xc.workspace

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Mikha's Book on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
