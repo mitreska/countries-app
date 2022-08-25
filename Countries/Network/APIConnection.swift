0ced9554eebbc3e0d06e10a0ed8ba8775f651d47
mitreska <estudio.mtp@gmail.com>
2022-08-25 13:16:13 -0300

    bugfix: url encoded name
    replacing special characters

--- Countries/Network/APIConnection.swift
+++ Countries/Network/APIConnection.swift
@@ -25,7 +25,15 @@ final class APIConnection {
     }
     
     func getDescription(countryName: String) -> DataResponsePublisher<CountryDescription> {
-        let url = "\(baseURLInfo)\(countryName)"
+        var encodedName = countryName
+        zip(["á","é","â","ã","ú","ó","í","õ","ô","ç"," "], ["%C3%A1","e","a","%C3%A3","%C3%BA","%C3%B3","%C3%AD","%C3%B5","%C3%B4","%C3%A7","%20"]).forEach({
+            encodedName = encodedName.replacingOccurrences(of: $0, with: $1, options: .literal)
+        })
+        
+        let url = "\(baseURLInfo)\(encodedName)"
+        
+        print(url)
+        
         return AF.request(url, method: .get)
             .validate(statusCode: 200..<400)
             .publishDecodable(type: CountryDescription.self)
